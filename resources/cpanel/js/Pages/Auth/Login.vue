2b80b9d90a5b340bb727ef4404e87b44d01d1818
Huỳnh Phúc <huynhngochoangphuc@gmail.com>
2021-04-20 12:07:48 +0700

    chore: Code format.

--- resources/cpanel/js/Pages/Auth/Login.vue
+++ resources/cpanel/js/Pages/Auth/Login.vue
@@ -1,87 +1,109 @@
 <template>
-    <breeze-validation-errors class="mb-4" />
+  <breeze-validation-errors class="mb-4" />
 
-    <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-        {{ status }}
+  <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+    {{ status }}
+  </div>
+
+  <form @submit.prevent="submit">
+    <div>
+      <breeze-label for="email" value="Email" />
+      <breeze-input
+        id="email"
+        v-model="form.email"
+        autocomplete="username"
+        autofocus
+        class="mt-1 block w-full"
+        required
+        type="text"
+      />
     </div>
 
-    <form @submit.prevent="submit">
-        <div>
-            <breeze-label for="email" value="Email" />
-            <breeze-input id="email" type="text" class="mt-1 block w-full" v-model="form.email" required autofocus autocomplete="username" />
-        </div>
+    <div class="mt-4">
+      <breeze-label for="password" value="Password" />
+      <breeze-input
+        id="password"
+        v-model="form.password"
+        autocomplete="current-password"
+        class="mt-1 block w-full"
+        required
+        type="password"
+      />
+    </div>
 
-        <div class="mt-4">
-            <breeze-label for="password" value="Password" />
-            <breeze-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" />
-        </div>
+    <div class="block mt-4">
+      <label class="flex items-center">
+        <breeze-checkbox v-model:checked="form.remember" name="remember" />
+        <span class="ml-2 text-sm text-gray-600">Remember me</span>
+      </label>
+    </div>
 
-        <div class="block mt-4">
-            <label class="flex items-center">
-                <breeze-checkbox name="remember" v-model:checked="form.remember" />
-                <span class="ml-2 text-sm text-gray-600">Remember me</span>
-            </label>
-        </div>
+    <div class="flex items-center justify-end mt-4">
+      <inertia-link
+        v-if="canResetPassword"
+        :href="route('password.request')"
+        class="underline text-sm text-gray-600 hover:text-gray-900"
+      >
+        Forgot your password?
+      </inertia-link>
 
-        <div class="flex items-center justify-end mt-4">
-            <inertia-link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                Forgot your password?
-            </inertia-link>
-
-            <breeze-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                Log in
-            </breeze-button>
-        </div>
-    </form>
+      <breeze-button
+        :class="{ 'opacity-25': form.processing }"
+        :disabled="form.processing"
+        class="ml-4"
+      >
+        Log in
+      </breeze-button>
+    </div>
+  </form>
 </template>
 
 <script>
-    import BreezeButton from '../../Components/Button'
-    import BreezeGuestLayout from "../../Layouts/Guest"
-    import BreezeInput from '../../Components/Input'
-    import BreezeCheckbox from '../../Components/Checkbox'
-    import BreezeLabel from '../../Components/Label'
-    import BreezeValidationErrors from '../../Components/ValidationErrors'
+import BreezeButton from '../../Components/Button';
+import BreezeGuestLayout from '../../Layouts/Guest';
+import BreezeInput from '../../Components/Input';
+import BreezeCheckbox from '../../Components/Checkbox';
+import BreezeLabel from '../../Components/Label';
+import BreezeValidationErrors from '../../Components/ValidationErrors';
 
-    export default {
-        layout: BreezeGuestLayout,
+export default {
+  layout: BreezeGuestLayout,
+  components: {
+    BreezeButton,
+    BreezeInput,
+    BreezeCheckbox,
+    BreezeLabel,
+    BreezeValidationErrors
+  },
 
-        components: {
-            BreezeButton,
-            BreezeInput,
-            BreezeCheckbox,
-            BreezeLabel,
-            BreezeValidationErrors
-        },
+  props: {
+    auth: Object,
+    canResetPassword: Boolean,
+    errors: Object,
+    status: String
+  },
 
-        props: {
-            auth: Object,
-            canResetPassword: Boolean,
-            errors: Object,
-            status: String,
-        },
+  data() {
+    return {
+      form: this.$inertia.form({
+        email: '',
+        password: '',
+        remember: false
+      })
+    };
+  },
 
-        data() {
-            return {
-                form: this.$inertia.form({
-                    email: '',
-                    password: '',
-                    remember: false
-                })
-            }
-        },
-
-        methods: {
-            submit() {
-                this.form
-                    .transform(data => ({
-                        ... data,
-                        remember: this.form.remember ? 'on' : ''
-                    }))
-                    .post(this.route('cpanel.login'), {
-                        onFinish: () => this.form.reset('password'),
-                    })
-            }
-        }
+  methods: {
+    submit() {
+      this.form
+        .transform(data => ({
+          ...data,
+          remember: this.form.remember ? 'on' : ''
+        }))
+        .post(this.route('cpanel.login'), {
+          onFinish: () => this.form.reset('password')
+        });
     }
+  }
+};
 </script>
