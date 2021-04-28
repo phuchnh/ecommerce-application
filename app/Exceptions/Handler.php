eebfd167867ddb5b506f64514623cf6d79e55fe1
Huỳnh Phúc <huynhngochoangphuc@gmail.com>
2021-04-28 13:36:00 +0700

    chore: Handle error 403.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\UnauthorizedException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -34,8 +35,8 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (UnauthorizedException $e) {
+            abort(403);
         });
     }
 }
